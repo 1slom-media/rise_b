4ed5a53b26c6cf8dc0e48ac20b911632bec8e1f3
1slom_media <islombektagayev1@gmail.com>
2023-10-30 14:02:17 +0500

    ad domain

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:3004', 'http://localhost:3005', 'https://rise-shopping.uz', 'https://admin.rise-shopping.uz','https://admin.rise-shopping.com'], // Allow requests only from this origin
+        origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:3004', 'http://localhost:3005', 'https://rise-shopping.uz', 'https://admin.rise-shopping.uz','https://admin.rise-shopping.com','https://admin.rise-shopping.com'], // Allow requests only from this origin
         credentials: true, // Allow credentials (cookies, headers, etc.)
     })
 );
