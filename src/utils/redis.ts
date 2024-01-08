0f361b45c6b994d693d9d07985e9b03f2b5780b6
1slom_media <islombektagayev1@gmail.com>
2024-01-09 01:10:22 +0500

    new server

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -3,7 +3,7 @@ import Redis from 'ioredis';
 const redis = new Redis({
   port: 6379, // Redis port
   host: 'localhost', // Redis host
-  password: 'islom_01',
+  password: 'Pm2dPkDE%rEF',
 });
 
 
