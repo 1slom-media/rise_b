d1661f340c3d3464f2dae56ab6fc63a1d4a388a5
1slom-media <islombektagayev1@gmail.com>
2024-02-11 10:54:09 +0500

    add graophql api

---
+++ src/resolvers.ts
@@ -0,0 +1,134 @@
+import { AppDataSource } from "./data-source"
+import { CategoryEntity } from "./entities/category"
+import { CompanyEntity } from "./entities/company";
+import { ProductsEntity } from "./entities/products";
+import { SubCategoryEntity } from "./entities/sub_category";
+
+export const resolvers = {
+    Query: {
+        category: async () => {
+            const category = await AppDataSource.getRepository(CategoryEntity).find({
+                relations: {
+                    sub_category: true,
+                    products: true
+                }
+            })
+            return category;
+        },
+        company: async () => {
+            const company = await AppDataSource.getRepository(CompanyEntity).find({
+                relations: {
+                    country: true,
+                    admin: true,
+                    products: true,
+                    brand: true
+                }
+            })
+            return company;
+        },
+        sub_category: async () => {
+            const sub_category = await AppDataSource.getRepository(SubCategoryEntity).find({
+                relations: {
+                    category: true,
+                    size: true,
+                    products: true
+                }, order: { id: "ASC" }
+            })
+            return sub_category;
+        },
+        products: async (_, { brand, category, subcategory, color, size, min, max, country, sort, summ, skip, take, company }) => {
+            let brandIds: number[] = [];
+            let categoryIds: number[] = [];
+            let subcategoryIds: number[] = [];
+
+
+            if (brand) {
+                if (Array.isArray(brand)) {
+                    brandIds = brand.map(b => parseInt(b));
+                } else if (typeof brand === 'string') {
+                    brandIds = brand.split(',').map(b => parseInt(b));
+                } else {
+                    brandIds = [+brand];
+                }
+            }
+            const sizes = String(size).split(',');
+            if (category) {
+                if (Array.isArray(category)) {
+                    categoryIds = category.map(b => parseInt(b));
+                } else if (typeof category === 'string') {
+                    categoryIds = category.split(',').map(b => parseInt(b));
+                } else {
+                    categoryIds = [+category];
+                }
+            } if (subcategory) {
+                if (Array.isArray(subcategory)) {
+                    subcategoryIds = subcategory.map(b => parseInt(b));
+                } else if (typeof subcategory === 'string') {
+                    subcategoryIds = subcategory.split(',').map(b => parseInt(b));
+                } else {
+                    subcategoryIds = [+subcategory];
+                }
+            }
+
+            let query = AppDataSource.getRepository(ProductsEntity)
+                .createQueryBuilder('products')
+                .leftJoinAndSelect('products.category', 'category')
+                .leftJoinAndSelect('products.brand', 'brand')
+                .leftJoinAndSelect('products.parametrs', 'parametrs')
+                .leftJoinAndSelect('products.sub_category', 'sub_category')
+                .leftJoinAndSelect('products.prices', 'prices').leftJoinAndSelect('products.company', 'company').leftJoinAndSelect('products.charactics', 'charactics').leftJoinAndSelect('products.country', 'country');
+
+            if (brandIds.length > 0) {
+                query = query.andWhere("brand.id IN (:...brandIds)", { brandIds });
+            }
+            if (skip && take) {
+                query = query.skip(+take * (+skip - 1)).take(+take)
+            }
+            if (categoryIds.length > 0) {
+                query = query.andWhere("category.id IN (:...categoryIds)", { categoryIds });
+            } if (subcategoryIds.length > 0) {
+                query = query.andWhere("sub_category.id IN (:...subcategoryIds)", { subcategoryIds });
+            }
+            if (country && +country > 0) {
+                query = query.andWhere('country.id = :country_id', { country_id: country });
+            }
+            if (company && +company > 0) {
+                query = query.andWhere('company.id = :company_id', { company_id: company });
+            }
+            if (min && max && +min >= 0 && +max > 0) {
+                query = query
+                    .innerJoin('products.prices', 'price')
+                    .andWhere('price.price >= :min_price', { min_price: +min })
+                    .andWhere('price.price <= :max_price', { max_price: +max });
+            }
+            if (size) {
+                query = query.andWhere(':sizes::character varying[] <@ products.size', { sizes })
+            }
+            if (sort && sort == "aksiya" || sort == "ommabop" || sort == "new") {
+                query = query.andWhere('products.status=:status', { status: sort })
+            }
+            if (summ && summ === "max") {
+                query = query.innerJoin('products.prices', 'price').orderBy('CAST(prices.price AS DECIMAL)', 'DESC');
+            }
+            if (summ && summ === "min") {
+                query = query.innerJoin('products.prices', 'price').orderBy('CAST(prices.price AS DECIMAL)', 'ASC');
+            }
+
+            const productList = await query.orderBy('products.id', 'DESC').getMany();
+            if (color) {
+                const filteredProducts = productList.filter(product =>
+                    product.parametrs.some(parametr => parametr.color === color)
+                );
+
+                const lastFilter = filteredProducts.map(product => {
+                    const matchingParams = product.parametrs.filter(parametr => parametr.color === color);
+                    return { ...product, parametrs: matchingParams };
+                });
+
+                return lastFilter
+            } else {
+                return productList
+            }
+        }
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,32 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import session from 'express-session';
+import http from "http";
 import passport from './utils/gf';
 import { AppDataSource } from './data-source';
 import router from './routes';
 import path from 'path';
-
-const app: Application = express();
+import fs from 'fs';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { resolvers } from './resolvers';
 
 const PORT = process.env.PORT || 5000
+
+const app: Application = express();
+const httpServer = http.createServer(app);
+
+const schema = fs.readFileSync(path.resolve(process.cwd(), "src", "graphql", "schema.gql"));
+
+
+const server = new ApolloServer({
+  typeDefs: gql`${schema}`,
+  resolvers,
+  context: async ({ req }) => ({ token: req.headers.token }),
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -22,5 +35,9 @@ AppDataSource.initialize().then((): void => console.log("connected")).catch((err
 app.use('/static', express.static(path.join(process.cwd(), 'uploads')))
 app.use(router);
 
-app.listen(PORT, (): void => console.log(`http://localhost:${PORT}`));
-
+(async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise<void>((resolve) => httpServer.listen({ port: PORT }, resolve));
+  console.log(`🚀 Server ready at http://localhost:5000${server.graphqlPath}`);
+})();
